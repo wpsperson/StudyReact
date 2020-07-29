f482a014039f72055570ac939a7164bda6e0ef06
wangps <wangps@glodon.com>
2020-07-29 15:30:46 +0800

    JS  rest Operator

--- src/App.js
+++ src/App.js
@@ -55,19 +55,18 @@ return (
 );
 }
 
-const List = (props)=> props.list.map( (item)=>
-( 
-<Item key={item.objectID} item={item}/>
-)  )
+const List = (props)=> props.list.map( 
+  ({objectID, ...item})=> <Item key={objectID} {...item}/>
+)
 
-const Item = ({item})=>(
+const Item = ({url, title, author, num_comments, points})=>(
   <div>
   <span>
-  <a href={item.url}>{item.title}</a>
+  <a href={url}>{title}</a>
   </span>
-  <span>{item.author}</span>
-  <span>{item.num_comments}</span>
-  <span>{item.points}</span>
+  <span>{author}</span>
+  <span>{num_comments}</span>
+  <span>{points}</span>
   </div>
 )
 
