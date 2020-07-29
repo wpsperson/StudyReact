9621d95df1e1713c100397ee5b701383d35340ea
wangps <wangps@glodon.com>
2020-07-29 15:47:26 +0800

    inline handler: <button type="button" onClick={ ()=>onRemoveItem(item) }>

--- src/App.js
+++ src/App.js
@@ -65,28 +65,21 @@ const List = ({list, onRemoveItem})=> list.map(
   (item)=> <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem}/>
 )
 
-const Item = ({item, onRemoveItem})=>{
-
-  const handleRemoveItem = () => {
-    onRemoveItem(item);
-    };
-
-  return (
-    <div>
-    <span>
-    <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    <span>
-      <button type="button" onClick={handleRemoveItem}>
-        Dismiss
-      </button>
-    </span>
-    </div>
-  )
-}
+const Item = ({item, onRemoveItem})=>(
+  <div>
+  <span>
+  <a href={item.url}>{item.title}</a>
+  </span>
+  <span>{item.author}</span>
+  <span>{item.num_comments}</span>
+  <span>{item.points}</span>
+  <span>
+    <button type="button" onClick={ ()=>onRemoveItem(item) }>
+      Dismiss
+    </button>
+  </span>
+  </div>
+)
 
 const InputWithLabel = ({id, value, isFocused,  onChange, children}) => {
 
