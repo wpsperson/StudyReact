c8b5285cd68b91f5d2dc32ab8b59305418551b1b
wangps <wangps@glodon.com>
2020-08-01 12:05:11 +0800

    Restore： without style

--- src/App.js
+++ src/App.js
@@ -1,96 +1,15 @@
 import React from 'react';
 import axios from 'axios';
-//import styles from './App.module.css';
-import styled from 'styled-components';
-import { ReactComponent as Check } from './check.svg';
 
-
-const StyledContainer = styled.div`
-height: 100vw;
-padding: 20px;
-background: #83a4d4;
-background: linear-gradient(to left, #b6fbff, #83a4d4);
-color: #171212;
-`;
-const StyledHeadlinePrimary = styled.h1`
-font-size: 48px;
-font-weight: 300;
-letter-spacing: 2px;
-`;
-const StyledItem = styled.div`
-display: flex;
-align-items: center;
-padding-bottom: 5px;
-`;
-const StyledColumn = styled.span`
-padding: 0 5px;
-white-space: nowrap;
-overflow: hidden;
-white-space: nowrap;
-text-overflow: ellipsis;
-a {
-color: inherit;
-}
-width: ${props => props.width};
-`;
-const StyledButton = styled.button`
-background: transparent;
-border: 1px solid #171212;
-padding: 5px;
-cursor: pointer;
-transition: all 0.1s ease-in;
-&:hover {
-background: #171212;
-color: #ffffff;
-}
-`;
-//注意这里有继承关系
-const StyledButtonSmall = styled(StyledButton)`
-padding: 5px;
-`;
-const StyledButtonLarge = styled(StyledButton)`
-padding: 10px;
-`;
-const StyledSearchForm = styled.form`
-padding: 10px 0 20px 0;
-display: flex;
-align-items: baseline;
-`;
-
-const StyledLabel = styled.label`
-border-top: 1px solid #171212;
-border-left: 1px solid #171212;
-padding-left: 5px;
-font-size: 24px;
-`;
-const StyledInput = styled.input`
-border: none;
-border-bottom: 1px solid #171212;
-background-color: transparent;
-font-size: 24px;
-`;
-
-
-
-
-//const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
-const API_ENDPOINT = 'https://api.github.com/users/';
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const useSemiPersistentState = (key, initialState) => {
-  
-  const isMounted = React.useRef(false);
-
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
   );
 
   React.useEffect(() => {
-    if(!isMounted.current){
-      isMounted.current = true;
-    }else{
-      console.log("A");
-      localStorage.setItem(key, value);
-    }
+    localStorage.setItem(key, value);
   }, [value, key]);
 
   return [value, setValue];
@@ -113,7 +32,7 @@ const storiesReducer = (state, action) => {
       };
     case 'STORIES_FETCH_FAILURE':
       return {
-        data:[],
+        ...state,
         isLoading: false,
         isError: true,
       };
@@ -134,47 +53,31 @@ const App = () => {
     'search',
     'React'
   );
-  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
+
+  const [url, setUrl] = React.useState(
+    `${API_ENDPOINT}${searchTerm}`
+  );
 
   const [stories, dispatchStories] = React.useReducer(
     storiesReducer,
     { data: [], isLoading: false, isError: false }
   );
 
-    //采用 memoized function
-  const handleFetchStories = React.useCallback( async ()=>{
+  const handleFetchStories = React.useCallback(async () => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
+
     try {
-          //用axios 和 用 fetch 不同，如果404会进入catch中，不用再then中判断了。
-        let result = await axios.get(url);
-        //dispatchStories({ type: 'STORIES_FETCH_SUCCESS',  payload: result.data.hits, });
-        let id = result.data.id;
-        let name = result.data.login;
-        let html_url = result.data.html_url;
-        let story =     {
-          title: '<<The ' + name +' Tutorial>>',
-          url: html_url,
-          author: name,
-          num_comments: 2,
-          points: 5,
-          objectID: id,
-          };
-        let story2 =     {
-          title: '<<The ' + name +'2 Tutorial>>',
-          url: html_url,
-          author: name,
-          num_comments: 2,
-          points: 5,
-          objectID: id+1,
-          };
-        dispatchStories({ type: 'STORIES_FETCH_SUCCESS',  payload: [story, story2], }); 
-    } catch (error) {
-        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+      const result = await axios.get(url);
+
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
     }
-  }, [url] )
+  }, [url]);
 
-
-  //采用 memoized function
   React.useEffect(() => {
     handleFetchStories();
   }, [handleFetchStories]);
@@ -185,40 +88,60 @@ const App = () => {
       payload: item,
     });
   };
-  //输入的handler
+
   const handleSearchInput = event => {
     setSearchTerm(event.target.value);
   };
-  //提交按钮的handler
-  const handleSearchSubmit = (event) => {
+
+  const handleSearchSubmit = event => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+
     event.preventDefault();
   };
 
-
-
   return (
-    <StyledContainer >
-      <StyledHeadlinePrimary >My Hacker Stories</StyledHeadlinePrimary>
-      <SearchForm  
-          searchTerm={searchTerm} 
-          handleSearchInput={handleSearchInput} 
-          handleSearchSubmit={handleSearchSubmit} 
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
       />
+
       <hr />
+
       {stories.isError && <p>Something went wrong ...</p>}
 
       {stories.isLoading ? (
         <p>Loading ...</p>
       ) : (
-        <List
-          list={stories.data}
-          onRemoveItem={handleRemoveStory}
-        />
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
-    </StyledContainer>
+    </div>
   );
 };
+
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit,
+}) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+);
 
 const InputWithLabel = ({
   id,
@@ -238,9 +161,9 @@ const InputWithLabel = ({
 
   return (
     <>
-      <StyledLabel htmlFor={id}  >{children}</StyledLabel>
+      <label htmlFor={id}>{children}</label>
       &nbsp;
-      <StyledInput
+      <input
         ref={inputRef}
         id={id}
         type={type}
@@ -261,38 +184,19 @@ const List = ({ list, onRemoveItem }) =>
   ));
 
 const Item = ({ item, onRemoveItem }) => (
-  <StyledItem >
-    <StyledColumn width='40%'>
+  <div>
+    <span>
       <a href={item.url}>{item.title}</a>
-    </StyledColumn>
-    <StyledColumn width='30%'>{item.author}</StyledColumn>
-    <StyledColumn width='10%'>{item.num_comments}</StyledColumn>
-    <StyledColumn width='10%'>{item.points}</StyledColumn>
-    <StyledColumn width='10%'>
-      <StyledButtonSmall type="button" onClick={() => onRemoveItem(item)}>
-      <Check height="18px" width="18px" />
-      </StyledButtonSmall>
-    </StyledColumn>
-  </StyledItem>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
+  </div>
 );
-
-const SearchForm = ({searchTerm, handleSearchInput, handleSearchSubmit }) => {
-  return (
-    <StyledSearchForm  onSubmit={handleSearchSubmit} >
-    <InputWithLabel
-      id="search"
-      value={searchTerm}
-      isFocused
-      onInputChange={handleSearchInput}
-    >
-      <strong>Search:</strong>
-    </InputWithLabel>
-    <StyledButtonLarge  type="submit" disabled={!searchTerm} >
-      Submit
-    </StyledButtonLarge>
-    </StyledSearchForm>
-  )
-}
-
 
 export default App;
